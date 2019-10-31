140254bae713a360407d607e7d89da2f81f29a92
王浩宇 <wanghaoyu.66@bytedance.com>
2019-10-31 14:48:14 +0800

    添加B1

---
+++ TestDemo/TestDemo/B1ViewController.h
@@ -0,0 +1,17 @@
+//
+//  B1ViewController.h
+//  TestDemo
+//
+//  Created by 王浩宇 on 2019/10/31.
+//  Copyright © 2019 王浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface B1ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestDemo/TestDemo/B1ViewController.m
@@ -0,0 +1,32 @@
+//
+//  B1ViewController.m
+//  TestDemo
+//
+//  Created by 王浩宇 on 2019/10/31.
+//  Copyright © 2019 王浩宇. All rights reserved.
+//
+
+#import "B1ViewController.h"
+
+@interface B1ViewController ()
+
+@end
+
+@implementation B1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
